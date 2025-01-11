a3ae5037ec8e0941c83a6feeceea61d80220d65a
Markus Fehringer <2210859004@fh-burgenland.at>
2025-01-11 17:47:18 +0100

    rename entry class

---
+++ src/main/java/at/fehringer/authentication/ApplicationBasic.java
@@ -0,0 +1,14 @@
+package at.fehringer.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBasic {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBasic.class, args);
+    }
+
+}
+
